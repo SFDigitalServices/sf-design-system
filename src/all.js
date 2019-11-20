0b63f2ab2ccc420872f7cb602ebe6e38f1fa403d
Josh Rubenoff <1328849+jrubenoff@users.noreply.github.com>
2019-11-20 11:19:51 -0800

    Form styles + typography mixins (#21)
    * Add form field markup and context data
    
    * Move fields to field-types folder
    
    * Move container components to subdir
    
    * Add hero banner and pagination markup
    
    * Re-add file field context data
    
    * [unrelated] Fix pattern scaffolding styles
    
    * Update typography mixins, add font size variable
    
    * Add contain-1090 and clearfix mixins
    
    * Add border-radius variable
    
    * Add global styles for typography and box-sizing
    
    * Add pagination and hero-banner styles
    
    * Remove duplicate file field context
    
    * Add file field JS
    
    * Add small-text mixin
    
    * Convert other typography mixins to rems
    
    * Add styles for form-group and help-block
    
    * Reorganize + add comments to form field CSS
    
    * Add cursor: pointer to pagination buttons
    
    * Add error validation to inputs
    
    * Fix styling bug with length- classes
    
    * Style “optional” label
    
    * Add meta viewport tag to preview layout
    
    * Add test page for typography mixins

--- src/all.js
+++ src/all.js
@@ -1,1 +1,12 @@
-console.log('test');
+$(document).ready(function(){
+
+	// Initialize 1000hz-bootstrap-validator
+	$("form").validator();
+
+	// File inputs
+	$("input[type='file']").change(function() {
+	  var file = $(this).val().replace(/C:\\fakepath\\/i, '');
+
+	  $(this).next('.file-custom').attr('data-filename', file);
+	});
+});

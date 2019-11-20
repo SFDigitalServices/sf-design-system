f5b7307fcb3961bff0a1e109e6e9fcc64eb5f0b8
Josh Rubenoff <joshua.rubenoff@sfgov.org>
2019-11-20 09:41:52 -0800

    Add file field JS

--- src/all.js
+++ src/all.js
@@ -1,1 +1,8 @@
-console.log('test');
+$(document).ready(function(){
+	// File inputs
+	$("input[type='file']").change(function() {
+	  var file = $(this).val().replace(/C:\\fakepath\\/i, '');
+
+	  $(this).next('.file-custom').attr('data-filename', file);
+	});
+});

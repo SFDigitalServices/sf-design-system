ca01dea48361d5064b2dd8a0a2688bb6a0aa838d
Josh Rubenoff <joshua.rubenoff@sfgov.org>
2019-11-20 10:04:48 -0800

    Add error validation to inputs

--- src/all.js
+++ src/all.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+
+	// Initialize 1000hz-bootstrap-validator
+	$("form").validator();
+
 	// File inputs
 	$("input[type='file']").change(function() {
 	  var file = $(this).val().replace(/C:\\fakepath\\/i, '');

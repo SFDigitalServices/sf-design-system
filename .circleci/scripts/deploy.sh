d14d48f494ab87018d344dca60c5a3bcdddee056
Shawn Allen <shawn.allen@sfgov.org>
2020-02-11 15:14:37 -0800

    comment out @aekong's review site comment posting for now

--- .circleci/scripts/deploy.sh
+++ .circleci/scripts/deploy.sh
@@ -86,5 +86,5 @@ else
   COMMENT="review site: https://ci-${CIRCLE_BUILD_NUM}-sfdesignsystem.pantheonsite.io"
   OWNER="SFDigitalServices"
   REPO="sf-design-system"
-  curl -u aekong:$GH_ACCESS_TOKEN -H "Content-Type: application/json" -d '{"body":"'"$COMMENT"'"}' -X POST https://api.github.com/repos/$OWNER/$REPO/commits/$CIRCLE_SHA1/comments
+  # curl -u aekong:$GH_ACCESS_TOKEN -H "Content-Type: application/json" -d '{"body":"'"$COMMENT"'"}' -X POST https://api.github.com/repos/$OWNER/$REPO/commits/$CIRCLE_SHA1/comments
 fi

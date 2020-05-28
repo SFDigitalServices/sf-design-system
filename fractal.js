27de7a3137b085454629875d07db5897486d4759
Shawn Allen <shawnbot@users.noreply.github.com>
2020-05-27 17:44:03 -0700

    set default template context to {theme}, add markdown filter

--- fractal.js
+++ fractal.js
@@ -1,10 +1,22 @@
 const { join } = require('path')
+const theme = require('./src/theme')
+const defaultContext = { theme }
+
 /* Create a new Fractal instance and export it for use elsewhere if required */
 const fractal = module.exports = require('@frctl/fractal').create()
 
 fractal.set('project.title', 'San Francisco Design System')
 
-fractal.components.engine('@frctl/nunjucks')
+fractal.components.set('default.context', defaultContext)
+fractal.docs.set('default.context', defaultContext)
+
+const engine = require('@frctl/nunjucks')({
+  filters: {
+    markdownify: require('nunjucks-markdown-filter')
+  }
+})
+
+fractal.components.engine(engine)
 fractal.components.set('ext', '.html')
 
 fractal.web.theme(require('@frctl/mandelbrot')({
